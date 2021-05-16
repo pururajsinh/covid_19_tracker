c25ccc992f9dd9f99e3b97c06e5b1fcbb0ca2465
pururajsinh <pururajsolanki111@gmail.com>
2021-05-16 00:29:55 -0500

    initial commit with project skeleton

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div>
+            <h1>Cards</h1>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CountryPicker() {
+    return (
+        <div>
+            <h1>Country Picker</h1>
+        </div>
+    )
+}
+
+export default CountryPicker
