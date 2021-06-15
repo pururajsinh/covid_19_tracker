b36dc32a1ca323dfdb03745efbf2a2052893b1ff
pururajsinh <pururajsolanki111@gmail.com>
2021-06-15 17:15:31 -0500

    Added color change

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -44,8 +44,8 @@ function Chart({ data: { confirmed, recovered, deaths }, country }) {
           {
             label: "People",
             backgroundColor: [
-              "rgba(0, 255, 0, 0.5)",
               "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
               "rgba(255, 0, 0, 0.5)",
             ],
             data: [confirmed.value, recovered.value, deaths.value],
